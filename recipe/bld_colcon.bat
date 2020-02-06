cca2d636f585f43acd2204d9dd7078d3366f37ef
seanyen <seanyen@microsoft.com>
2020-02-06 11:51:05 -0800

    fix.

--- recipe/bld_colcon.bat
+++ recipe/bld_colcon.bat
@@ -15,12 +15,30 @@ colcon build ^
     --event-handlers console_cohesion+
 if errorlevel 1 exit 1
 
-del %LIBRARY_PREFIX%\.colcon_install_layout
-del %LIBRARY_PREFIX%\COLCON_IGNORE
-del %LIBRARY_PREFIX%\local_setup.bat
-del %LIBRARY_PREFIX%\local_setup.ps1
-del %LIBRARY_PREFIX%\setup.bat
-del %LIBRARY_PREFIX%\setup.ps1
-del %LIBRARY_PREFIX%\_local_setup_util_bat.py
-del %LIBRARY_PREFIX%\_local_setup_util_ps1.py
-rd /s /q %LIBRARY_PREFIX%\__pycache__
+if exist %LIBRARY_PREFIX%\.colcon_install_layout (
+    del %LIBRARY_PREFIX%\.colcon_install_layout
+)
+if exist %LIBRARY_PREFIX%\COLCON_IGNORE (
+    del %LIBRARY_PREFIX%\COLCON_IGNORE
+)
+if exist %LIBRARY_PREFIX%\local_setup.bat (
+    del %LIBRARY_PREFIX%\local_setup.bat
+)
+if exist %LIBRARY_PREFIX%\local_setup.ps1 (
+    del %LIBRARY_PREFIX%\local_setup.ps1
+)
+if exist %LIBRARY_PREFIX%\setup.bat (
+    del %LIBRARY_PREFIX%\setup.bat
+)
+if exist %LIBRARY_PREFIX%\setup.ps1 (
+    del %LIBRARY_PREFIX%\setup.ps1
+)
+if exist %LIBRARY_PREFIX%\_local_setup_util_bat.py (
+    del %LIBRARY_PREFIX%\_local_setup_util_bat.py
+)
+if exist %LIBRARY_PREFIX%\_local_setup_util_ps1.py (
+    del %LIBRARY_PREFIX%\_local_setup_util_ps1.py
+)
+if exist %LIBRARY_PREFIX%\__pycache__\ (
+    rd /s /q %LIBRARY_PREFIX%\__pycache__\
+)
