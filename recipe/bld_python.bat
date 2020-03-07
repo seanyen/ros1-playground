458a24b120d4a0418eb40ead8c5eb5b136c95722
seanyen <seanyen@microsoft.com>
2020-03-07 14:30:25 -0800

    fix.

--- recipe/bld_python.bat
+++ recipe/bld_python.bat
@@ -6,5 +6,5 @@ pushd %SRC_DIR%\%PKG_NAME%\src\work
 
 mkdir %LIBRARY_PREFIX%\lib\site-packages
 
-pip setup.py install --prefix=%LIBRARY_PREFIX%
+%PYTHON% setup.py install --prefix=%LIBRARY_PREFIX%
 if errorlevel 1 exit 1
